23489d634bf10ca6094a609e5a008a95a030f731
Usman Javed <usmanjaved0816@gmail.com>
2024-11-04 16:02:59 +0500

    wrapp up crash course

--- index.js
+++ index.js
@@ -6,30 +6,77 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-
-
 const app = http.createServer((req, res)=> {
-    console.log(req.url);
-    if (req.url === '/') {
-        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err , content)=> {
+
+
+
+    // res.writeHead(200, {
+    //     'content-type' : 'text/html'
+    // })
+
+    // if (req.url === '/') {
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err , content)=> {
            
             
-            if(err) {
-                throw err
-            }
-            res.end(content)
-        })
-    } else if (req.url === '/about') {
-        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err , content)=> {
-            if(err) {
-                throw err
-            }
-            res.end(content)
-        })
-    }
+    //         if(err) {
+    //             throw err
+    //         }
+    //         res.end(content)
+    //     })
+    // } else if (req.url === '/about') {
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err , content)=> {
+    //         if(err) {
+    //             throw err
+    //         }
+    //         res.end(content)
+    //     })
+    // }
 
- 
-//  res.end('<h1> http module in node js ....  </h1>')
+
+
+
+
+let filePath = path.join(__dirname, 'public' , req.url ==='/' ? 'index.html' : req.url);
+let contentType = 'text/html';
+let ext = path.extname(filePath)
+if(!ext) {
+    filePath += '.html'
+}
+
+switch(ext) {
+    case '.css':
+        contentType='text/css';
+        break;
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        default :
+        contentType = 'text/html'    
+}
+
+
+fs.readFile(filePath, (err, content)=> {
+    if (err) {
+        fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data)=>{
+            if(err) {
+                res.writeHead(500)
+                res.end('Error ...')
+            } else {
+                res.writeHead(404, {
+                    'conten-Type' : contentType
+                })
+                res.end(data)
+            }
+        })
+        
+    }
+    else {
+        res.writeHead(200 , {
+        'content-Type' : contentType    
+        })
+        res.end(content)
+    }
+})
 })
 
 const PORT = process.env.PORT || 3000;
@@ -38,3 +85,7 @@ app.listen(PORT, ()=> {
     console.log(`listing on port ${PORT}`);
     
 })
+
+
+
+
